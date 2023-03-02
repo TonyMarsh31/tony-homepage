cb4f600d8694ab5885ccdfe8fb78f071cc69707e
tonymarsh <tonymarsh0331@gmail.com>
2023-03-02 23:45:45 +0800

    :beers: Add a navigation bar; change some style setting of index.js.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="blue" p={3} mb={6} align="center">
-        <p>
-          Hi, I'm Sun Tao
-          <br />
-          welcome to my personal website.
-        </p>
+      <Box
+        borderRadius="lg"
+        p={3}
+        mb={6}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      >
+        Hi, I'm Sun Tao, welcome to my personal website.
       </Box>
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
@@ -15,8 +37,34 @@ const Page = () => {
             About Me
           </Heading>
           <p>
-            Student based in Shanghai , on my way to become a Software Engineer.
+            {' '}
+            Student based in Shanghai , on my way to become a Software Engineer.{' '}
           </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            BorderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/profile.jpg"
+              alt="Profile Image"
+              borderRadius="full"
+              width={100}
+              height={100}
+            />
+          </Box>
         </Box>
       </Box>
     </Container>
