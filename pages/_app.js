d5080a1f5edbcdf89de1cd72739196aa78f9860b
tonymarsh <tonymarsh0331@gmail.com>
2023-03-03 17:34:35 +0800

    :sparkles: darkMode support

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
+import theme from '../lib/theme'
 
 const Website = ({ Component, PageProps, router }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout router={router}>
         <Component {...PageProps} key={router.router} />
       </Layout>
